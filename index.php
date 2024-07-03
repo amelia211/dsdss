d6e618f4a8ddfec1b10a86eacfd423127b4955ad
amelia211 <136762846+amelia211@users.noreply.github.com>
2024-07-03 13:37:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<!-- BINTURONG -->
+
+<html lang="en">
+
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>LOCAL HOOKUP</title>
+      <!-- Linking SwiperJS CSS -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+      <link rel="stylesheet" href="style.css">
+    </head>
+  <body>
+    <div class="container swiper">
+        <div class="slider-wrapper">
+            <div class="card-list swiper-wrapper">
+                <div class="card-item swiper-slide">
+                    <img src="images/bg-1.png" alt="User Image" class="user-image">
+                    <h2 class="user-name">James Tok</h2>
+                    <p class="bugil-santuy">LIVE CAMS</p>
+                    <a href="#"><button class="kontol">Message</button></a>
+                </div>
+
+                <div class="card-item swiper-slide">
+                    <img src="images/bg-1.png" alt="User Image" class="user-image">
+                    <h2 class="user-name">James Tok</h2>
+                    <p class="bugil-santuy">LIVE CAMS</p>
+                    <a href="#"><button class="kontol">Message</button></a>
+                </div>
+
+                <div class="card-item swiper-slide">
+                    <img src="images/bg-1.png" alt="User Image" class="user-image">
+                    <h2 class="user-name">James Tok</h2>
+                    <p class="bugil-santuy">LIVE CAMS</p>
+                    <a href="#"><button class="kontol">Message</button></a>
+                </div>
+
+                <div class="card-item swiper-slide">
+                    <img src="images/bg-1.png" alt="User Image" class="user-image">
+                    <h2 class="user-name">James Tok</h2>
+                    <p class="bugil-santuy">LIVE CAMS</p>
+                    <a href="#"><button class="kontol">Message</button></a>
+                </div>
+
+                <div class="card-item swiper-slide">
+                    <img src="images/bg-1.png" alt="User Image" class="user-image">
+                    <h2 class="user-name">James Tok</h2>
+                    <p class="bugil-santuy">LIVE CAMS</p>
+                    <a href="#"><button class="kontol">Message</button></a>
+                </div>
+
+                <div class="card-item swiper-slide">
+                    <img src="images/bg-1.png" alt="User Image" class="user-image">
+                    <h2 class="user-name">James Tok</h2>
+                    <p class="bugil-santuy">LIVE CAMS</p>
+                    <a href="#"><button class="kontol">Message</button></a>
+                </div>
+
+                <div class="card-item swiper-slide">
+                    <img src="images/bg-1.png" alt="User Image" class="user-image">
+                    <h2 class="user-name">James Tok</h2>
+                    <p class="bugil-santuy">LIVE CAMS</p>
+                    <a href="#"><button class="kontol">Message</button></a>
+                </div>
+
+                <div class="card-item swiper-slide">
+                    <img src="images/bg-1.png" alt="User Image" class="user-image">
+                    <h2 class="user-name">James Tok</h2>
+                    <p class="bugil-santuy">LIVE CAMS</p>
+                    <a href="#"><button class="kontol">Message</button></a>
+                </div>
+           </div>
+
+           <div class="swiper-pagination"></div>
+           <div class="swipe-slide-button swiper-button-prev"></div>
+           <div class="swipe-slide-button swiper-button-next"></div>
+        </div>
+      </div>
+       
+      <!-- Linking SwiperJS script -->
+      <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+      <!-- Linking custom sript -->
+      <script src="script.js"></script>
+  </body>
+</html>
